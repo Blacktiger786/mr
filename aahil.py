3b8d227ab7761f47e8bdca3831602a74569e5909
Blacktiger786 <63770663+Blacktiger786@users.noreply.github.com>
2020-04-18 11:31:28 +0500

    Update aahil.py

--- aahil.py
+++ aahil.py
@@ -122,9 +122,9 @@ def login():
 		os.system('clear')
 		print logo
 		print 42*"\033[1;96m="
-		print('\033[1;96m[☆] \x1b[1;91mAPNA FACEBOOK ACCOUNT LOGIN KREIN \x1b[1;96m[☆]' )
-		id = raw_input('\033[1;96m[+] \x1b[0;34mID/Email \x1b[1;91m: \x1b[1;92m')
-		pwd = raw_input('\033[1;96m[+] \x1b[0;34mPassword \x1b[1;91m: \x1b[1;92m')
+		print('\033[1;96m[⚡] \x1b[1;93mLogin your new id \x1b[1;93m[⚡]' )
+		id = raw_input('\033[1;963m[+] \x1b[0;34mEnter ID/Email \x1b[1;93m: \x1b[1;93m')
+		pwd = raw_input('\033[1;93m[+] \x1b[0;34mEnter Password \x1b[1;93m: \x1b[1;93m')
 		tik()
 		try:
 			br.open('https://m.facebook.com')
